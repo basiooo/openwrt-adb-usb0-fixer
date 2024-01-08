eb6c904475419eaa148a0aaacec043092ac7f429
Bagas Julianto <bagasjulianto212@gmail.com>
2024-01-08 11:05:36 +0700

    add echo

--- fix_usb0.sh
+++ fix_usb0.sh
@@ -2,8 +2,10 @@
 
 if [ -e /etc/usb-mode.json ]; then
     mv /etc/usb-mode.json /etc/usb-mode.json.backup
+    echo "Success Fixing USB0"
     echo "backup file: /etc/usb-mode.json.backup"
     echo "Reboot to apply changes"
 else
+    echo "Failed Fixing USB0"
     echo "/etc/usb-mode.json not found in the system"
 fi
